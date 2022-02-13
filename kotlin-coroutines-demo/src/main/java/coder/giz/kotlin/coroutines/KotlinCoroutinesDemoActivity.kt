f311fdea68471e7add417c23082f365273ffc894
GizFei <1150847818@qq.com>
2022-02-13 15:28:03 +0800

    [koroutines] Create Kotlin coroutines demo module.

---
+++ kotlin-coroutines-demo/src/main/java/coder/giz/kotlin/coroutines/KotlinCoroutinesDemoActivity.kt
@@ -0,0 +1,11 @@
+package coder.giz.kotlin.coroutines
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class KotlinCoroutinesDemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_coroutines_demo)
+    }
+}

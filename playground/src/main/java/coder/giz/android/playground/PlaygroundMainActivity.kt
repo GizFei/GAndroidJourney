4b9c4742ac01623def1ba53462a1cc6b3ff9b8c7
GizFei <1150847818@qq.com>
2022-03-21 23:05:22 +0800

    [playground] Create Playground module.

---
+++ playground/src/main/java/coder/giz/android/playground/PlaygroundMainActivity.kt
@@ -0,0 +1,11 @@
+package coder.giz.android.playground
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PlaygroundMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playground_main)
+    }
+}
